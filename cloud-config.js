351d9905e791ec91657295e26f1bbe21ccd14058
ammartaha687 <ammartaha687@gmail.com>
2026-08-20 17:14:09 +0300

    Delete cloud-config.js

--- cloud-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    supabaseUrl: "https://pbyosypgcakawfiwayap.supabase.co/",
-    supabaseKey: "sb_publishable_jR4jm7CG9zY9A57qjd7eDA_FkZPs2gM",
-    deviceCode: "SHOP-PC-001"
-};
